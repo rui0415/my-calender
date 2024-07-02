29828988191e6aa2e5f6f537b8f5e66da06bec82
rui0415 <rui20030415@gmail.com>
2024-07-02 20:56:26 +0900

    日付切り替わり機能追加

--- components/Calender.tsx
+++ components/Calender.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from "react";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 import style from "./style/calender.module.css"
 
 const day = (year: number, month: number) => new Date(year, month + 1, 0).getDate();
@@ -30,25 +31,47 @@ export default function Calender() {
     
     for (let day = 1; day <= numDays; day++) {
         const weekDayIndex = week(year, month, day);
-        // const isToday = day === now.getDate() && month === now.getMonth() && year === now.getFullYear();
-        const isToday = day === today.getDate();
+        const isToday = day === today.getDate() && month === today.getMonth() && year === today.getFullYear();
         days.push(<div key={`day-${day}`} className={style.day} style={{color: isToday ? "yellow" : ""}}>{day}</div>);
     }
+
+    const prevMonth = () => {
+        setMonth(prev => prev === 0 ? 11 : prev - 1);
+        if (month === 0) {
+            setYear(prev => prev - 1);
+        }
+    };
+
+    const nextMonth = () => {
+        setMonth(prev => prev === 11 ? 0 : prev + 1);
+        if (month === 11) {
+            setYear(prev => prev + 1);
+        }   
+    };
+
+    const backToday = () => {
+        setYear(today.getFullYear());
+        setMonth(today.getMonth());
+    };
 
     return (
         <div className={style.container}>
             <div className={style.board}>
 
                 <div className={style.header}>
-                    <div>
-                        <span className={style.month}>{month+1}月 </span>
-                        <span>{year}</span>
+                    <div className={style.date}>
+                        <span className={style.month}>{month+1} 月</span>
+                        <span> {year}</span>
                     </div>
 
-                    <div>
-                        <span>prev </span>
-                        <span>next</span>
+                    <div className={style.button}>
+                        <button onClick={prevMonth} style={{marginRight:"3rem"}}><FaAngleLeft /></button>
+                        <button onClick={nextMonth}><FaAngleRight /></button>
                     </div>
+
+                    <button className={style.backToday} onClick={backToday}>
+                        {today.getDay()}
+                    </button>
                 </div>
 
                 <div className={style.week}>
